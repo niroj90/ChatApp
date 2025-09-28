7d18be0536942e17049f96e6116954e88a20c282
Niroj <snero90@gmail.com>
2025-09-28 14:46:35 +0530

    chat controller added

---
+++ ChatApp/Controllers/ChatController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChatController : ControllerBase
+    {
+        private readonly ILogger<ChatController> _logger;
+        public ChatController(ILogger<ChatController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost(Name = "CreateChatSession")]
+        public async Task<IActionResult> Create()
+        {
+            return Ok();
+        }
+
+    }
+}
